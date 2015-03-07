3d20af45b7f9ae58a006f52681849d3a674c45d7
Jordan Quan <quan.jordan@gmail.com>
2015-03-07 13:51:28 -0500

    tree and meadows

--- Assets/Scripts/HexGraph.cs
+++ Assets/Scripts/HexGraph.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class HexGraph : MonoBehaviour {
 
 	public GameObject hex;
+
 	public int rows;
 	public int cols;
 
 	private float hexWidth;
 	private float hexHeight;
 
-	private System.Random rand = new System.Random();
+	//private System.Random rand = new System.Random();
 
 	void setSizes()
 	{
@@ -31,22 +32,12 @@ public class HexGraph : MonoBehaviour {
 				GO.transform.parent=this.transform;
 				Tile t = GO.GetComponent<Tile>();
 				t.coord = new Vector2(x,y);
-				int type = rand.Next (1, 4);
-				switch(type)
-				{
-					case 1:
-						GO.renderer.material.color = Color.green;
-						break;
-					case 2:
-						GO.renderer.material.color = Color.red;
-						break;
-					case 3:
-						GO.renderer.material.color = Color.blue;
-						break;
-				}
+				t.setInitialType();
 			}
 		}
 	}
+
+
 
 	void Start()
 	{
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -13,8 +13,40 @@ public class Tile : MonoBehaviour {
 
 	public Vector2 coord;
 	public List<GameObject> neighbours;
+	public GameObject tree;
+	public GameObject meadow;
 	//private LandType myType;
 	//private System.Random rand = new System.Random();
+
+	public void setInitialType()
+	{	
+		int type = Random.Range (1, 4);
+		GameObject GO;
+		Vector3 scale;
+
+		switch(type)
+		{
+			case 1:
+				//renderer.material.color = Color.green;
+				break;
+			case 2:
+				//renderer.material.color = Color.red;
+				GO = (GameObject)Instantiate (tree, transform.position, Quaternion.identity);
+				scale = GO.transform.localScale;
+				GO.transform.parent=this.transform;
+				GO.transform.Translate(0f,0.1f,0f);
+				GO.transform.localScale = scale;
+				break;
+			case 3:
+				//renderer.material.color = Color.blue;
+				GO = (GameObject)Instantiate (meadow, transform.position, Quaternion.identity);
+				scale = GO.transform.localScale;
+				GO.transform.parent=this.transform;
+				GO.transform.Translate(0f,0.1f,0f);
+				GO.transform.localScale = scale;
+				break;
+		}
+	}
 
 	void Start()
 	{
